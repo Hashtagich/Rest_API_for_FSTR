9190520545419e69a61798858415426544061cac
Hashtagich <antonchikz@mail.ru>
2024-03-20 20:04:36 +0300

    Спринт 1. Написал серилиазаторы и представления для теста запуска API.

--- mountain_pass/the_pass/serializers.py
+++ mountain_pass/the_pass/serializers.py
@@ -2,22 +2,37 @@ from .models import Coord, Level, Pereval, Images, User
 from rest_framework import serializers
 
 
-class CoordSerializer(serializers.HyperlinkedModelSerializer):
-    pass
+class CoordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coord
+        fields = ['latitude', 'longitude', 'height']
 
 
-class LevelSerializer(serializers.HyperlinkedModelSerializer):
-    pass
+class LevelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Level
+        fields = ['winter', 'summer', 'autumn', 'spring']
 
 
-class PerevalSerializer(serializers.HyperlinkedModelSerializer):
-    pass
+class ImagesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Images
+        fields = ['data', 'title']
 
 
-class ImagesSerializer(serializers.HyperlinkedModelSerializer):
-    pass
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'fam', 'name', 'otc', 'phone']
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    pass
+class PerevalSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    coords = CoordSerializer()
+    level = LevelSerializer()
+    images = ImagesSerializer(many=True)
 
+    class Meta:
+        model = Pereval
+        fields = ['beauty_title', 'title', 'other_titles', 'connect', 'add_time',
+                  'user', 'coords', 'level', 'images', 'status']
--- mountain_pass/the_pass/views.py
+++ mountain_pass/the_pass/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+# from .models import *
+from .serializers import *
+
 
 # Create your views here.
+class UserViewset(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class CoordsViewset(viewsets.ModelViewSet):
+    queryset = Coord.objects.all()
+    serializer_class = CoordSerializer
+
+
+class LevelViewset(viewsets.ModelViewSet):
+    queryset = Level.objects.all()
+    serializer_class = LevelSerializer
+
+
+class ImageViewset(viewsets.ModelViewSet):
+    queryset = Images.objects.all()
+    serializer_class = ImagesSerializer
+
+
+class PerevalViewset(viewsets.ModelViewSet):
+    queryset = Pereval.objects.all()
+    serializer_class = PerevalSerializer
