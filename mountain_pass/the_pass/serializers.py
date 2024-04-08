c5645385eaaa730f55af637b251d44f9179747ed
Hashtagich <antonchikz@mail.ru>
2024-04-08 10:47:08 +0300

    Спринт 2. Добавил методы согласно ТЗ спринта 2.

--- mountain_pass/the_pass/serializers.py
+++ mountain_pass/the_pass/serializers.py
@@ -82,4 +82,19 @@ class PerevalSerializer(serializers.ModelSerializer):
 
         return pereval_new
 
+    def validate(self, data):
+        if self.instance is not None:
+            instance_user = self.instance.user
+            data_user = data.get('user')
+            validating_user_fields = [
+                instance_user.fam != data_user['fam'],
+                instance_user.name != data_user['name'],
+                instance_user.otc != data_user['otc'],
+                instance_user.phone != data_user['phone'],
+                instance_user.email != data_user['email'],
+            ]
+            if data_user is not None and any(validating_user_fields):
+                raise serializers.ValidationError({'Данные пользователя не могут быть изменены'})
+        return data
+
 
--- mountain_pass/the_pass/views.py
+++ mountain_pass/the_pass/views.py
@@ -1,5 +1,6 @@
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework import viewsets
+from django.http import JsonResponse
 from rest_framework.response import Response
 from .models import MyUser, Coord, Level, Images, Pereval
 from .serializers import CoordSerializer, UserSerializer, LevelSerializer, ImagesSerializer, PerevalSerializer
@@ -54,4 +55,48 @@ class PerevalViewSet(viewsets.ModelViewSet):
                 'id': None,
             })
 
+    def partial_update(self, request, *args, **kwargs):
+        pereval = self.get_object()
 
+        new_email = request.data.get('user', {}).get('email')
+        if new_email:
+            existing_user = MyUser.objects.filter(email=new_email).exclude(pk=pereval.user.pk)
+            if existing_user.exists():
+                return Response({
+                    'state': '0',
+                    'message': "Пользователь с таким email уже существует."
+                })
+
+        serializer = PerevalSerializer(pereval, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'state': '1',
+                'message': 'Запись успешно изменена'
+            })
+        else:
+            return Response({
+                'state': '0',
+                'message': serializer.errors
+            })
+
+
+class SubmitDataViewSet(viewsets.ViewSet):
+    def list(self, request):
+        user_email = request.query_params.get('user__email', None)
+        if user_email:
+            user = MyUser.objects.filter(email=user_email).first()
+            if user:
+                data_objects = Pereval.objects.filter(user=user)
+                serializer = PerevalSerializer(data_objects, many=True)
+                return Response(serializer.data)
+            else:
+                return Response({
+                    'message': 'Пользователь с такой почтой не найден',
+                    'data': []
+                })
+        else:
+            return Response({
+                'message': 'Параметр user__email обязателен',
+                'data': []
+            })
